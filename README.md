d3f5264d8c9b4475d837c9ed355ebb3b349fbb1f
Justin Hopkins <hopkinsju@gmail.com>
2014-02-08 18:39:27 -0600

    inital commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This project was developed using Flora wearable electronics platform from Adafruit (http://www.adafruit.com/products/659) and the super tiny accelerometer/magnetometer sensor based on the LM303 (http://www.adafruit.com/products/1247). They power NeoPixel strips (http://www.adafruit.com/products/1426) mounted on the underside of roller skates.
+
+You'll need some Arduino libraries to get started, and I'd recommend the Adafruit Arduino IDE so that it will work with the Flora/Gemma right away.
+
+Check this out for links to the Adafruit Arduino IDE and other getting started stuff: http://learn.adafruit.com/getting-started-with-flora/download-software
+
+LM303 Library: https://github.com/adafruit/Adafruit_LSM303DLHC
+NeoPixel Library: https://github.com/adafruit/Adafruit_NeoPixel
+Universal Sensor Library: https://github.com/adafruit/Adafruit_Sensor
+
+All this stuff is brand new to me and I'm just playing around with it. Right now we can change the color of the NeoPixel strips based on compass heading. In the future I'll be adding other modes. Color splash on foot fall, brightness changes based on speed, etc.
+
+I really appreciate all the cool project made by Becky Stern lately. Super inspirational.
---
+++ SkateLights.ino
@@ -0,0 +1,223 @@
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
+
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+  // init the lights
+  strip.begin();
+  strip.setBrightness(64);
+  strip.show(); // Initialize all pixels to 'off'
+  
+  // Get serial ready for debugging
+  // This can probably go when it's time for Gemma
+  Serial.begin(9600);
+  
+  // Check for the accelerometer
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+  
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+}
+
+void loop() {
+//  // Some example procedures showing how to display to the pixels:
+//  colorWipe(strip.Color(255, 0, 0), 50); // Red
+//  colorWipe(strip.Color(0, 255, 0), 50); // Green
+//  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+//  // Send a theater pixel chase in...
+//  theaterChase(strip.Color(127, 127, 127), 50); // White
+//  theaterChase(strip.Color(127,   0,   0), 50); // Red
+//  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+//
+//  rainbow(20);
+//  rainbowCycle(20);
+//  theaterChaseRainbow(50);
+
+// This is how to set a single pixel to a color.
+// arg 1 = pixel num (starting at 0)
+// arg 2-4 = RGB value
+//strip.setPixelColor(1, 255, 0, 0);
+
+// This is another way that stores a 32bit color value to a variable name first for easy reuse
+//uint32_t magenta = strip.Color(255, 0, 255);
+//uint32_t yellow = strip.Color(255, 255, 0);
+//strip.setPixelColor(2, magenta);
+
+// Set some brigtness? 0-255 values
+//strip.setBrightness(64);
+
+/* Get a new sensor event */ 
+sensors_event_t event; 
+accel.getEvent(&event);
+
+
+int zAccel = event.acceleration.z;
+
+
+if (zAccel < 3) {
+  //fadeDown(64, 0, 64);
+  Serial.print("Fading!");
+}
+
+//if (event.acceleration.y > 0) {
+//  for(uint16_t i=0; i<strip.numPixels(); i++) {
+//    strip.setPixelColor(i, yellow);
+//  }
+//}
+
+  mag.getEvent(&event);
+  
+  float Pi = 3.14159;
+  
+  // Calculate the angle of the vector y,x
+  float heading = (atan2(event.magnetic.y,event.magnetic.x) * 180) / Pi;
+  
+  // Normalize to 0-360
+  if (heading < 0)
+  {
+    heading = 360 + heading;
+  }
+//  Serial.print("Compass Heading: ");
+//  Serial.println(heading);
+//  Serial.print("Byte Value: ");
+//  Serial.println(ByteHeading(heading));
+//  Serial.print("Wheel Value: ");
+//  Serial.println(Wheel(ByteHeading(heading)));
+//  delay(500);
+
+colorWipe(Wheel(ByteHeading(heading)), 1);
+
+// This updates the strip
+//strip.show();
+
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+     
+      delay(wait);
+     
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+        }
+        strip.show();
+       
+        delay(wait);
+       
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        }
+    }
+  }
+}
+
+// Take a compass heading and return a 0-255 value. 
+// Handy for sending to the Wheel() func to get back a color
+uint32_t ByteHeading(uint32_t Heading) {
+  float adj = 255.0/360.0;
+  return (unsigned int) (Heading * adj);
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+void fadeDown(uint32_t highVal, uint32_t lowVal, uint8_t rate) {
+  for(int i = highVal; i > lowVal; i--) {
+    strip.setBrightness(i);
+
+    strip.show();
+    delay(rate);
+  }
+}
